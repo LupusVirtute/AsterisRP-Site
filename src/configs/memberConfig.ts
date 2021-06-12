75850545968183db980bc7b1aafb122fdf27b58e
PuccyDestroyerxXx <mrcake.2014@wp.pl>
2021-06-12 17:00:29 +0200

    Style update

--- src/configs/memberConfig.ts
+++ src/configs/memberConfig.ts
@@ -4,45 +4,59 @@ export let members: member[]  = [
         about: `Lokal : Przystojny Chłopak`,
         image: `
         https://cdn.discordapp.com/avatars/486261740468699146/a_1cf51645dec48842453733ec19346037.gif?size=2048
-        `
+        `,
+        discord: ''
+
     },
     {
         nick: '!Majkel',
         about: ``,
         image: `
         https://cdn.discordapp.com/avatars/346295074465775626/54ee128109425d12eb9bd148b0b44f13.png?size=4096
-        `
+        `,
+        discord: ''
+
     },
     {
         nick: `Bila`,
         about: `Bila`,
         image: `
         https://cdn.discordapp.com/avatars/610226883178790917/4909d83acb370675ab227110300e7a29.png?size=4096
-        `
+        `,
+        discord: ''
+
     },
     {
         nick: `J.ohny ;(`,
         about: `J.ohny ;(`,
         image: `
         https://cdn.discordapp.com/avatars/680477939133055052/cfbdd6f4b1e1a99243d0f1896b34e4d0.png?size=4096        `
+        ,
+        discord: ''
+
     },
     {
         nick: `OG morsulek`,
         about: ``,
         image: `
         https://cdn.discordapp.com/avatars/531785950775476236/a_292d262117fc4ed781cc361cdc204619.gif?size=4096
-        `
+        `,
+        discord: ''
+
     },
     {
         nick: `xolaaxx_`,
         about: `J.ohny ;(`,
         image: `
         https://cdn.discordapp.com/avatars/511960928548290560/5fb344e0c27ecd2efda12242634a064f.png?size=4096
-        `
+        `,
+        discord: ''
+
     },
 ]
 export interface member {
     about: string,
     nick: string,
-    image: string
+    image: string,
+    discord: string
 }
